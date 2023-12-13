929465d9e0d3af3dc9097051b5e69f3bff6ca4ff
erenaltun0 <93876518+erenaltun0@users.noreply.github.com>
2023-12-14 01:58:59 +0300

    Yazar Listeleme işlemi yapıldı. Yazarlar için gerekli alanların eklenmesi yapıldı.

--- EntityLayer/Concrete/Author.cs
+++ EntityLayer/Concrete/Author.cs
@@ -18,6 +18,16 @@ namespace EntityLayer.Concrete
         public string AuthorImage { get; set; }
         [StringLength(250)]
         public string AuthorAbout { get; set; }
+		[StringLength(50)]
+        public string AuthorTitle { get; set; }
+		[StringLength(100)]
+        public string AuthorShort { get; set; }
+		[StringLength(50)]
+        public string Mail { get; set; }
+		[StringLength(50)]
+        public string Password { get; set; }
+		[StringLength(20)]
+        public string PhoneNumber { get; set; }
         public ICollection<Blog> Blogs { get; set; }
     }
 }
